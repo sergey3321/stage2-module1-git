d8d0b3476ec3c7945b0b660c7c30f129bcf39da3
sergey3321 <87018903+sergey3321@users.noreply.github.com>
2024-07-24 15:47:10 +0300

    Update Repo.java

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/sergey3321/stage2_module1_git_Sergey/tree/master";
+    public static String REPO_LINK = "https://github.com/sergey3321/stage2_module1_git_Sergey/tree/main";
 }
